1fff08cf0dfdda330c67ef3fbb25fafd5f7c8f2d
fcamel <fcamel@64G.itri.org.tw>
2010-12-01 22:50:58 +0800

    Add django and fab bash completion.

--- .bash_completions/fab_completion.bash
+++ .bash_completions/fab_completion.bash
@@ -5,7 +5,7 @@ _fab_completion() {
 #    /usr/bin/which fab || return 0
 
     # Fabfile in this folder?
-    [[ -e fabfile.py ]] || return 0
+    [[ -e fabfile.py ]] || [[ -e ../fabfile.py ]] || [[ -e ../../fabfile.py ]] || return 0
 
     local cur="${COMP_WORDS[COMP_CWORD]}"
 
