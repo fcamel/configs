e87046a9dd33b1a2f3af74e42cf7299caa899955
fcamel <fcamel@gmail.com>
2012-03-02 21:22:27 +0800

    vim update

--- .vim/template/production.py
+++ .vim/template/production.py
@@ -4,19 +4,19 @@
 import sys
 import optparse
 
-def main(args):
+def main():
     '''\
     %prog [options]
     '''
-    return 0
-
-
-if __name__ == '__main__':
     parser = optparse.OptionParser(usage=main.__doc__)
     options, args = parser.parse_args()
 
     if len(args) != 0:
         parser.print_help()
-        sys.exit(1)
+        return 1
 
-    sys.exit(main(args))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
