268b6ee2e10d8ea1bbabb2ee831bed93e6d8ffe1
fcamel <fcamel@gmail.com>
2017-02-25 23:38:23 +0800

    .vim/template/bash.sh: add $TMP and remove $TMP at exit.

--- .vim/template/bash.sh
+++ .vim/template/bash.sh
@@ -5,7 +5,8 @@
 #--------------------------------------------------------------------
 
 cd $(dirname $0)
-ROOT=$(pwd)
+readonly ROOT=$(pwd)
+readonly TMP=${TMPDIR:-/tmp}/$(basename $0).tmp.$$
 
 #--------------------------------------------------------------------
 # Helper functions.
@@ -21,33 +22,56 @@ usage()
 EOF
 }
 
+clean_up()
+{
+  rm -f $TMP > /dev/null 2>&1
+}
+
+# echo colorful texts.
+# $1: 0-7, the color code.
+# $2: the text.
+cecho()
+{
+  echo -e "\n\03$1[1;33m$2\033[m"
+}
+
 run()
 {
-  echo -e "\n\033[1;33m> Run $@\033[m\n"
+  cecho 3 "> Run $@"
+  echo
   eval time $@  # Must use eval to process single quote.
 }
 
-#--------------------------------------------------------------------
-# Parse arguments.
+main()
+{
+  # Parse arguments.
+  while getopts "h" OPTION
+  do
+    case $OPTION in
+      h)
+        usage
+        exit 1
+        ;;
+      ?)
+        usage
+        exit 2
+        ;;
+    esac
+  done
+
+  # Keep the rest arguments in $@.
+  shift $((OPTIND-1))
+
+  # Main.
+  # TODO
+}
+
 #--------------------------------------------------------------------
 
-while getopts "h" OPTION
-do
-  case $OPTION in
-    h)
-      usage
-      exit 1
-      ;;
-    ?)
-      usage
-      exit 2
-      ;;
-  esac
-done
+# Clean up when receiving signals (including Ctrl+C).
+trap "clean_up; exit 1" 1 2 3 13 15
 
-# Keep the rest arguments in $@.
-shift $((OPTIND-1))
+main $@
 
-#--------------------------------------------------------------------
-# Main
-#--------------------------------------------------------------------
+clean_up
+exit 0
